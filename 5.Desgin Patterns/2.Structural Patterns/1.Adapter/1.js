8f84ee764e8882fbb5f93de1b7c13202124b72c1
Alireza <alireza.gholami403@gmail.com>
2021-09-12 23:52:37 +0430

    feat: add Adapter design pattern

--- 5.Desgin Patterns/2.Structural Patterns/1.Adapter/1.js
+++ 5.Desgin Patterns/2.Structural Patterns/1.Adapter/1.js
@@ -93,7 +93,7 @@ adapter.attach()
 
 
 //************** */
-
+/* 
 class Adaptee {
     specificRequest() {
         return 'Hong Kong Outlet';
@@ -113,6 +113,62 @@ class TargetAdaptee {
 
 // Remember to new adaptation class
 let ta = new TargetAdaptee();
-ta.request();
+ta.request(); */
 
-class 
+
+
+class Color {
+    constructor() {
+        this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
+    getColor() {
+        return this.color
+    }
+}
+
+// const newColor = new Color();
+// console.log('old',newColor.getColor());
+
+class ColorAdapter {
+    constructor() {
+        this.colorInstance = new Color()
+    }
+    getColor() {
+        const color = this.colorInstance.getColor();
+        console.log(color);
+        const red = this.hexToDecimal(color.slice(1, 3))
+        const green = this.hexToDecimal(color.slice(3, 5))
+        const blue = this.hexToDecimal(color.slice(5))
+        const rgbaColor = `rgba(${red},${green},${blue},1)`;
+        return rgbaColor
+    }
+
+    hexToDecimal(hex) {
+        return parseInt(hex, 16)
+    }
+};
+
+
+
+// it just works with rgba
+class Paint {
+    constructor(color) {
+        this.color = color
+    }
+
+    makeItClear(alpha = 0.5) {
+        const [r, g, b] = this.color.slice(5).split(',');
+        this.color = `rgba(${r},${g},${b},${alpha})`;
+        console.log(this.color);
+    }
+
+    doSomeStuff() {
+        //...
+    }
+}
+
+const newColor2 = new ColorAdapter()
+const selectedColor = newColor2.getColor()
+
+const paint = new Paint(selectedColor)
+paint.makeItClear()
